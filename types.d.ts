7a98caeb84f571d0ac8a486cb4b9ed110c455888
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-10-14 10:21:18 -0400

    Initial commit

--- types.d.ts
+++ types.d.ts
@@ -2,7 +2,17 @@ import { ActionOnEventConfigs } from "trans-render/froop/types";
 import {IBE} from 'be-enhanced/types';
 
 export interface EndUserProps extends IBE{
-
+    of?: Array<DispatchStatement>
+    Of?: Array<DispatchStatement>
 }
 
 export type DispatchStatement = string;
+
+export interface DispatchRule{
+    dispatchOn?: string,
+    dispatchOnPropChange?: string,
+    eventType?: string,
+    bubbles?: boolean,
+    composed?: boolean,
+    replace?: boolean,
+}
